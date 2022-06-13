45f94527b1e04cf894be0f6ba68f28bf91c1c078
Don Kim <donkdh1@gmail.com>
2022-06-13 10:40:48 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
-const http = require('http');
-const express = require('express');
-const path = require('path');
-const app = express();
-app.use(express.json());
-app.use(express.static("express"));
-// default URL for website
-app.use('/', function(req,res){
-    res.sendFile(path.join(__dirname+'/public/index.html'));
-    //__dirname : It will resolve to your project folder.
-  });
-const server = http.createServer(app);
-const port = 3000;
-server.listen(port);
-console.debug('Server listening on port ' + port);
+const requestListener = function (req, res) {
+    fs.readFile(__dirname + "/public/index.html")
+        .then(contents => {
+            res.setHeader("Content-Type", "text/html");
+            res.writeHead(200);
+            res.end(contents);
+        })
+        .catch(err => {
+            res.writeHead(500);
+            res.end(err);
+            return;
+        });
+};
